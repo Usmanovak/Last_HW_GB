bf3416f154f74b42e39f7785864c69d1e4060ced
Karina <usmkarin@gmail.com>
2023-05-09 21:03:24 +0700

    Написала метод, который считает количество элементов массива, длина которых не превышает 3-х символов

--- Last_HW/Program.cs
+++ Last_HW/Program.cs
@@ -10,3 +10,16 @@ using static System.Console;
 Clear();
 string [] array1 = new string[5] {"hello", "7", "hi", "117", "people"};
 string [] array2 = new string [array1.Length];
+
+void NewArray(string [] array1, string [] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
